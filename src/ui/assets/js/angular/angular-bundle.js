4d59709c271855e2813090d998f4f55af2de214b
bghoshdastid_asa <bikramaditya.ghoshdastidar@stonybrook.edu>
2016-07-22 11:04:01 -0400

    Removed unnecessary things for now

--- src/ui/assets/js/angular/angular-bundle.js
+++ src/ui/assets/js/angular/angular-bundle.js
@@ -40,6 +40,19 @@ AngularApp.run(["$rootScope", "$state", function ($rootScope, $state)
 {
     $state.go("home");
 }]);
+AngularApp.service("IPCService", function IPCService()
+{
+    const self = this;
+    
+    const IpcClient = require("electron-ipc-tunnel/client").default;
+    
+    const client = new IpcClient();
+    
+    self.send = function (channel, request)
+    {
+        return client.send(channel, request);
+    }
+});
 AngularApp.service("AuthService", ["$q", "$window", function ($q, $window)
 {
     const self = this;
@@ -58,116 +71,6 @@ AngularApp.service("IdentityService", function ()
         return !!self.currentUser;
     };
 });
-AngularApp.service("IPCService", function IPCService()
-{
-    const self = this;
-    
-    const IpcClient = require("electron-ipc-tunnel/client").default;
-    
-    const client = new IpcClient();
-    
-    self.send = function (channel, request)
-    {
-        return client.send(channel, request);
-    }
-});
-AngularApp.service("ModalService", ["$q", "$http", "$compile", "$rootScope", function ($q, $http, $compile, $rootScope)
-{
-    var exports = this;
-
-    var ModalInstance = function (element, options)
-    {
-        var _instance = this;
-
-        // Fields
-        _instance.element = element;
-        _instance.state = "default";
-
-        _instance.onOpen = options.onOpen;
-        _instance.onClose = options.onClose;
-
-        // Init the modal
-        _instance.element.modal({
-            show: false
-        });
-
-        // Width fix
-        _instance.element.width(options.width);
-
-        // Event Handlers
-        _instance.element.on("show", function ()
-        {
-            // Margin fix
-            _instance.element.css("margin-left", -(_instance.element.width() / 2));
-
-            if (_instance.onOpen)
-                _instance.onOpen();
-        });
-        _instance.element.on("hidden", function ()
-        {
-            if (_instance.onClose)
-                _instance.onClose();
-        });
-
-        // Open and Close functions
-        _instance.open = function ()
-        {
-            _instance.element.modal("show");
-        };
-        _instance.close = function ()
-        {
-            _instance.element.modal("hide");
-        };
-    };
-
-    // Create modal from Template URL
-    exports.createModal = function (templateUrl, options)
-    {
-        var def = $q.defer();
-
-        // Get the template markup from the URL provided
-        $http.get(templateUrl)
-            .success(function (response)
-            {
-                var element = angular.element(response);
-
-                var scope = $rootScope.$new(true);
-                $compile(element)(scope);
-
-                angular.element("#content-container").append(element);
-
-                var modalInstance = new ModalInstance(element, options || {width: 600});
-
-                def.resolve(modalInstance);
-            });
-
-        return def.promise;
-    };
-
-    // Store for all the global modals
-    exports.modals = {};
-
-    exports.waitUntilReady = function (modalName)
-    {
-        var def = $q.defer();
-
-        var watch = $rootScope.$watch(function()
-        {
-            return !!exports.modals[modalName];
-        }, function()
-        {
-            def.resolve(exports.modals[modalName]);
-            watch();
-        });
-
-        return def.promise;
-    };
-
-    // Global modals init function
-    exports.initGlobalModals = function ()
-    {
-    };
-}]);
 AngularApp.service("HeartbeatTestService", function HeartbeatTestService()
 {
     const self = this;
@@ -1915,6 +1818,103 @@ AngularApp.service("HeartbeatTestService", function HeartbeatTestService()
     
     }
 });
+AngularApp.service("ModalService", ["$q", "$http", "$compile", "$rootScope", function ($q, $http, $compile, $rootScope)
+{
+    var exports = this;
+
+    var ModalInstance = function (element, options)
+    {
+        var _instance = this;
+
+        // Fields
+        _instance.element = element;
+        _instance.state = "default";
+
+        _instance.onOpen = options.onOpen;
+        _instance.onClose = options.onClose;
+
+        // Init the modal
+        _instance.element.modal({
+            show: false
+        });
+
+        // Width fix
+        _instance.element.width(options.width);
+
+        // Event Handlers
+        _instance.element.on("show", function ()
+        {
+            // Margin fix
+            _instance.element.css("margin-left", -(_instance.element.width() / 2));
+
+            if (_instance.onOpen)
+                _instance.onOpen();
+        });
+        _instance.element.on("hidden", function ()
+        {
+            if (_instance.onClose)
+                _instance.onClose();
+        });
+
+        // Open and Close functions
+        _instance.open = function ()
+        {
+            _instance.element.modal("show");
+        };
+        _instance.close = function ()
+        {
+            _instance.element.modal("hide");
+        };
+    };
+
+    // Create modal from Template URL
+    exports.createModal = function (templateUrl, options)
+    {
+        var def = $q.defer();
+
+        // Get the template markup from the URL provided
+        $http.get(templateUrl)
+            .success(function (response)
+            {
+                var element = angular.element(response);
+
+                var scope = $rootScope.$new(true);
+                $compile(element)(scope);
+
+                angular.element("#content-container").append(element);
+
+                var modalInstance = new ModalInstance(element, options || {width: 600});
+
+                def.resolve(modalInstance);
+            });
+
+        return def.promise;
+    };
+
+    // Store for all the global modals
+    exports.modals = {};
+
+    exports.waitUntilReady = function (modalName)
+    {
+        var def = $q.defer();
+
+        var watch = $rootScope.$watch(function()
+        {
+            return !!exports.modals[modalName];
+        }, function()
+        {
+            def.resolve(exports.modals[modalName]);
+            watch();
+        });
+
+        return def.promise;
+    };
+
+    // Global modals init function
+    exports.initGlobalModals = function ()
+    {
+    };
+}]);
 AngularApp.component("homeComponent", {
     controller: "HomeController as Home",
     templateUrl: "templates/app/home/Home.template.html"
