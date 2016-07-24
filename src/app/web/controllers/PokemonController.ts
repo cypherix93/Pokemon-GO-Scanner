71ae9af142eb83c3b83559b157428ae64de2f0e3
cypherix93 <bikram.ghosh93@gmail.com>
2016-07-24 12:27:28 -0400

    Heartbeat map working with unique sorted markers for better performance.

--- src/app/web/controllers/PokemonController.ts
+++ src/app/web/controllers/PokemonController.ts
@@ -12,13 +12,13 @@ import {PokeIOWorker} from "../workers/PokeIOWorker";
 export class PokemonController
 {
     @Post("/getMapPokemons")
-    public async getMapPokemons(@Req() request: Request)
+    public async getMapPokemons(@Req() request:Request)
     {
         var latitude = parseFloat(request.body.latitude);
         var longitude = parseFloat(request.body.longitude);
 
         var heartbeats = await PokeIOWorker.getHeartbeatMapWithCoordinates(latitude, longitude);
-        var cells = _.flatten(heartbeats.map(x => x.cells));
+        var cells = _.flatten(heartbeats.map(x => x.cells)) as any[];
 
         var mapPokemons = _.flatten(cells.map(x => x.MapPokemon)) as any[];
         var wildPokemons = _.flatten(cells.map(x => x.WildPokemon)) as any[];
@@ -47,9 +47,25 @@ export class PokemonController
             markers.push(pokemonMarker);
         }
 
+        // Get unique markers
+        var sortedMarkers = markers.sort((x, y) =>
+        {
+            var a = x.id;
+            var b = y.id;
+
+            if (a < b)
+                return -1;
+
+            if (a > b)
+                return 1;
+
+            return 0;
+        });
+        var uniqueMarkers = _.sortedUniqBy(sortedMarkers, "id");
+
         return {
             success: true,
-            data: markers
+            data: uniqueMarkers
         };
     }
 }
