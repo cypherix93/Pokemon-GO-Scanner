383302cd2866aaf22d5c515d44cbf5ddc9d93425
cypherix93 <bikram.ghosh93@gmail.com>
2016-07-25 22:38:52 -0400

    Added a ping method to prevent the API session from terminating.

--- src/app/core/PokeIOApplication.ts
+++ src/app/core/PokeIOApplication.ts
@@ -3,12 +3,32 @@ import _ = require("lodash");
 
 import {PokeIO} from "./io/PokeIO";
 import {ArgsHelper} from "./helpers/ArgsHelper";
-import {PlayerProfile} from "./models/PlayerProfile";
-import {GeocoderHelper} from "./helpers/GeocoderHelper";
+import {Logger} from "./helpers/Logger";
+import Timer = NodeJS.Timer;
 
 export class PokeIOApplication
 {
     private static _io:PokeIO;
+
+    private static pingInterval:Timer;
+    private static pingIntervalTime = 60000;
+
+
+    public static async init():Promise<PokeIO>
+    {
+        var args = ArgsHelper.getArgs();
+
+        var io = new PokeIO();
+
+        await io.init(args.username, args.password, args.location, args.provider);
+
+        PokeIOApplication._io = io;
+
+        // On a 1 minute interval, ping Niantic to make sure they don't drop our session
+        PokeIOApplication.pingInterval = setInterval(() => PokeIOApplication.ping(), PokeIOApplication.pingIntervalTime);
+
+        return io;
+    }
 
     public static async getIO():Promise<PokeIO>
     {
@@ -21,32 +41,19 @@ export class PokeIOApplication
     public static resetIO()
     {
         PokeIOApplication._io = undefined;
+
+        clearInterval(PokeIOApplication.pingInterval);
     }
 
-    public static async init():Promise<PokeIO>
+    public static async ping()
     {
-        var args = ArgsHelper.getArgs();
+        var io = await PokeIOApplication.getIO();
 
-        var io = new PokeIO();
+        // Just get the API endpoint as a ping
+        io.player.apiEndpoint = await io.getApiEndpoint();
 
-        await io.init(args.username, args.password, args.location, args.provider);
+        await io.getProfile();
 
-        PokeIOApplication._io = io;
-
-        // Logger.info(`Current location: ${io.player.location.name}`);
-        // Logger.info(`Latitude / Longitude: ${io.player.location.latitude} ${io.player.location.longitude}`);
-        //
-        // var profile = await io.getProfile();
-        //
-        // Logger.info(`Username: ${profile.username}`);
-        // Logger.info(`Team: ${profile.team}`);
-        //
-        // Logger.info(`Poke Storage: ${profile.storage.pokemon}`);
-        // Logger.info(`Item Storage: ${profile.storage.items}`);
-        //
-        // Logger.info(`Pokecoin: ${profile.pokecoins}`);
-        // Logger.info(`Stardust: ${profile.stardust}`);
-
-        return io;
+        Logger.debug(`API Pinged at ${new Date().toISOString()}`);
     }
 }
